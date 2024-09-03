86cb9a47ca50852913d4e9ba5e466c03e239ef48
thweookhine <mathweookhine@gmail.com>
2024-09-03 22:39:36 +0630

    Add Auth Middleware

---
+++ auth/src/errors/not-authorized-err.ts
@@ -0,0 +1,17 @@
+import { CustomError } from "./custom-err";
+
+export class NotAuthorizedError extends CustomError {
+    statusCode = 404;
+
+    constructor() {
+        super('Not Authorized');
+        // Call pnly When we are writing typescript and trying to extend a class that is built in to language.
+        Object.setPrototypeOf(this, NotAuthorizedError.prototype)
+    }
+
+    serializeErrors(){
+        return ([
+            {'message': 'Not Authorized'}
+        ])
+    }
+}
