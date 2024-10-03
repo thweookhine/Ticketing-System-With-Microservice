851d9aff73264277a1baa13d0958f6cfc2878d43
thweookhine <mathweookhine@gmail.com>
2024-10-03 21:55:55 +0630

    Add Sign In

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,5 +1,15 @@
 import "bootstrap/dist/css/bootstrap.css"
+import buildClient from "../api/build-client";
 
-export default ({Component, pageProps}) => {
-    return <Component {...pageProps}/>
+const AppComponent = ({Component, pageProps}) => {
+    return (<div>
+        <h1>Header</h1>
+        <Component {...pageProps}/>
+    </div>);
 }
+
+AppComponent.getInitialProps = () => {
+    
+}
+
+export default AppComponent
