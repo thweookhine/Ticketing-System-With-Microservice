74e66e7f173259081ee0ff02369bd73f50dad6de
thweookhine <mathweookhine@gmail.com>
2024-11-04 21:22:33 +0630

    Test

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -1,5 +1,6 @@
 import nats from 'node-nats-streaming'
 
+console.clear()
 const stan = nats.connect('ticketing','abc',{
     url: 'http://localhost:4222'
 })
@@ -11,4 +12,14 @@ const stan = nats.connect('ticketing','abc',{
 // Function(2nd arg) will be executed after successfully connected to NATS Streaming Server
 stan.on('connect', () =>{
     console.log('Publisher connected to NATS')
+    const data = JSON.stringify({
+        id: 123,
+        title: 'concert',
+        price: 20
+    })
+
+    //Callback func (3rd arg) will be invoked after we publish the data
+    stan.publish('ticket:created', data, () => {
+        console.log('Event Published')
+    })
 })
