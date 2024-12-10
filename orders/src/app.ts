258ed62e7e1f32dbf3ffcf70b94b738dff4f626d
thweookhine <mathweookhine@gmail.com>
2024-12-10 22:32:23 +0630

    Orders depl

---
+++ orders/src/app.ts
@@ -0,0 +1,28 @@
+import { errorHandler, NotFoundError, currentUser } from '@demotickets/common'
+import cookieSession from 'cookie-session'
+import 'express-async-errors';
+import express from 'express'
+import {json} from 'body-parser'
+import { createTicketRouter } from './routes/new'
+import { showTicketDetailRouter } from './routes/showDetail'
+import { showAllTicketsRouter } from './routes/showAll';
+import { updateTicketRouter } from './routes/update';
+const app = express()
+app.set('trust proxy',true)
+app.use(json())
+app.use(cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test'
+}))
+app.use(currentUser as any);
+app.use(createTicketRouter);
+app.use(showTicketDetailRouter);
+app.use(showAllTicketsRouter)
+app.use(updateTicketRouter)
+app.all('*',async (req,res) => {
+    throw new NotFoundError();
+})
+
+app.use(errorHandler as any)
+
+export {app}
