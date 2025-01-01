83cb657030e77adbcc4a1d8b7a03f3467974edfe
thweookhine <mathweookhine@gmail.com>
2025-01-01 22:13:03 +0630

    Cancel Order

--- orders/src/routes/__test__/cancelOrder.test.ts
+++ orders/src/routes/__test__/cancelOrder.test.ts
@@ -1,13 +1,14 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose"
 import request from 'supertest'
-import { app } from '../../app'
-import { Ticket } from '../../models/ticket'
-import { cookie } from 'express-validator'
+import { app } from "../../app"
+import { Ticket } from "../../models/ticket"
+import { OrderStatus } from "@demotickets/common"
+import { Order } from "../../models/order"
 
 it('has a route handler listening to /api/orders/:orderId', async () => {
     const orderId = new mongoose.Types.ObjectId()
     const response = await request(app)
-            .get(`/api/orders/${orderId}`)
+            .delete(`/api/orders/${orderId}`)
             .send({})
     expect(response.status).not.toEqual(404)
 })
@@ -15,7 +16,7 @@ it('has a route handler listening to /api/orders/:orderId', async () => {
 it('can only be accessed if the user is signed in', async() => {
     const orderId = new mongoose.Types.ObjectId()
     const response = await request(app)
-                                .get(`/api/orders/${orderId}`)
+                                .delete(`/api/orders/${orderId}`)
                                 .send({});
     expect(response.status).toEqual(401)
 })
@@ -24,7 +25,7 @@ it('returns an error if order is not fouond', async() => {
     const cookie = await global.signup()
     const orderId = new mongoose.Types.ObjectId()
     const response = await request(app)
-                                .get(`/api/orders/${orderId}`)
+                                .delete(`/api/orders/${orderId}`)
                                 .set('Cookie',cookie)
                                 .send({});
     expect(response.status).toEqual(404)
@@ -48,19 +49,17 @@ it('return error when fetching Order by Other User', async () => {
            ticketId: ticket.id
        })
     
-    // Fetch Order by Other User
+    // Delete Order by Other User
     const response = await request(app)
-                            .get(`/api/orders/${orderResponse.body.id}`)
+                            .delete(`/api/orders/${orderResponse.body.id}`)
                             .set('Cookie', await global.signup())
                             .send({})
                             .expect(401)
 })
 
-it('Successfully Fetch order by Order ID', async () => {
-    // Create User cookie
-    const user1 = await global.signup();
-    
+it('Successfully mark order as canceled', async () => {
     // Create One Ticket
+    const user1 = await global.signup();
     const ticket = Ticket.build({
         title: 'Ticket',
         price: 200
@@ -69,21 +68,22 @@ it('Successfully Fetch order by Order ID', async () => {
     await ticket.save();
 
     // Create Order   
-    const {body: order} = await request(app)
+    const {body: createdOrder} = await request(app)
        .post('/api/orders')
        .set('Cookie',user1)
        .send({
            ticketId: ticket.id
-       })
-    
-    // Fetch Order by Other User
-    const {body: fetchedOrder} = await request(app)
-                            .get(`/api/orders/${order.id}`)
-                            .set('Cookie', user1)
-                            .send({})
-                            .expect(200)
-    
-    expect(fetchedOrder.id).toEqual(order.id);
-    expect(fetchedOrder.ticket.id).toEqual(ticket.id);
+    })
 
+    await request(app)
+        .delete(`/api/orders/${createdOrder.id}`)
+        .set('Cookie',user1)
+        .send({})
+        .expect(204)
+
+    const updatedOrder = await Order.findById(createdOrder.id)
+    expect(updatedOrder!.id).toEqual(createdOrder.id)
+    expect(updatedOrder!.status).toEqual(OrderStatus.Cancelled)
 })
+
+it.todo('emits an order cancelled to event')
--- orders/src/routes/cancelOrder.ts
+++ orders/src/routes/cancelOrder.ts
@@ -1,8 +1,22 @@
 import express,{Request, Response} from 'express'
+import { Order } from '../models/order'
+import { NotAuthorizedError, NotFoundError, OrderStatus, requireAuth } from '@demotickets/common';
 const router = express.Router()
 
-router.delete('/api/orders/:orderId', async(req: Request, res: Response) => {
-        res.send({})
-})
+router.delete('/api/orders/:orderId',requireAuth, async(req: Request, res: Response) => {
+        const order = await Order.findById(req.params.orderId);
 
+        if(!order){
+                throw new NotFoundError()
+        }
+
+        if(order.userId != req.currentUser?.id){
+                throw new NotAuthorizedError();
+        }
+
+        order.status = OrderStatus.Cancelled;
+        await order.save();
+
+        res.status(204).send({order})
+})
 export {router as cancelOrderRouter}
